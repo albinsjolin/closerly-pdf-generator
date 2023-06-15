10ac69c9c803afdf61534c4e20091378e80dba54
Albin Sjölin <albinsjoelin1@gmail.com>
2023-06-15 21:32:43 +0200

    feat: add pdf generator

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const puppeteer = require("puppeteer");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  const body = req.body;
+
+  if (!body || !body.html) {
+    return res.status(400).json({ error: "HTML content is required." });
+  }
+
+  const htmlContent = body.html;
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
+    await page.setContent(htmlContent);
+
+    const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
+    await browser.close();
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+    res.status(500).json({ error: "Error generating PDF." });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
